3be6474ece6bac6551e75a2be5063b1cafb0b4ea
Rubin <rubinradhakrishnan@gmail.com>
2020-09-13 15:21:13 +0530

    Style the search input field

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <span>Let's build the amazon clone!🚀 </span>
-      {/* Header */}
+      <Header />
       {/* Home */}
     </div>
   );
