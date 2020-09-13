ec2f3cc1202de1dcdebad3e766c694cb3b687283
Rubin <rubinradhakrishnan@gmail.com>
2020-09-13 14:18:01 +0530

    Initial setup and cleaning procedure

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAKWT1YwxRtyXC9r9C8pzByFmh315rYX1k",
+  authDomain: "clone-b035e.firebaseapp.com",
+  databaseURL: "https://clone-b035e.firebaseio.com",
+  projectId: "clone-b035e",
+  storageBucket: "clone-b035e.appspot.com",
+  messagingSenderId: "140284323159",
+  appId: "1:140284323159:web:bd2bdea13d47fbe5234a12",
+  measurementId: "G-6B5SQ8E56Y",
+};
