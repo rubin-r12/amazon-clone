08f4758061fc607cdbc67f3c31a2585f4434ecd5
Rubin <rubinradhakrishnan@gmail.com>
2020-09-14 23:59:29 +0530

    Add the reducer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { StateProvider } from "./StateProvider";
+import { initialState } from "./reducer";
 
 ReactDOM.render(
   <React.StrictMode>
--- src/reducer.js
+++ src/reducer.js
@@ -2,3 +2,15 @@ export const initialState = {
   basket: [],
   //   user: null,
 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_BASKET":
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
+  }
+};
+
+export default reducer;
