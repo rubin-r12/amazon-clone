08d2db4c066c591d8ad0b1952b633ca1cf574e39
Rubin <rubinradhakrishnan@gmail.com>
2020-09-17 00:18:15 +0530

    Add the buttons in the login page

--- src/Login.js
+++ src/Login.js
@@ -12,6 +12,24 @@ function Login() {
           alt=""
         />
       </Link>
+      <div className="login__container">
+        <h1>Sign-in</h1>
+        <form action="">
+          <h5>E-mail Address</h5>
+          <input type="text" />
+          <h5>Password</h5>
+          <input type="password" />
+          <button className="login__signInButton">Sign In</button>
+        </form>
+        <p>
+          By signing-in you agree to Amazon-demo-clone conditions of Use & Sale.
+          Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+        <button className="login__registerButton">
+          Create your Amazon Account
+        </button>
+      </div>
     </div>
   );
 }
