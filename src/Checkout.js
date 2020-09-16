eca90724d2059a821addd4ddc289bad737e9ac67
Rubin <rubinradhakrishnan@gmail.com>
2020-09-16 22:27:25 +0530

    Map the Checkout Product from the checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./Checkout.css";
 import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -15,7 +18,16 @@ function Checkout() {
 
         <div>
           <h2 className="checkout__title">Your Shopping Basket</h2>
-          <CheckoutProduct />
+
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
       <div className="checkout__right">
