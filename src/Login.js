57e7ff3fbaa11938c87fc41916cbc6bd19548ccf
Rubin <rubinradhakrishnan@gmail.com>
2020-09-16 23:42:40 +0530

    Add the login page component

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <h2>I'm the login</h2>
+    </div>
+  );
+}
+
+export default Login;
