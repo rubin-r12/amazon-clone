cfaf7a59fd67809673d47f9fb229838f5e127035
Rubin <rubinradhakrishnan@gmail.com>
2020-09-16 21:59:40 +0530

    Add the checkout Product component

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
@@ -14,6 +15,7 @@ function Checkout() {
 
         <div>
           <h2 className="checkout__title">Your Shopping Basket</h2>
+          <CheckoutProduct />
         </div>
       </div>
       <div className="checkout__right">
---
+++ src/CheckoutProduct.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CheckoutProduct.css";
+
+function CheckoutProduct() {
+  return (
+    <div className="checkoutProduct">
+      <h2>I'm a checkoutProduct</h2>
+    </div>
+  );
+}
+
+export default CheckoutProduct;
