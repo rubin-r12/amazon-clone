4a68d59b47bca6ee5d29bd9c3076ef74ca5310e5
Rubin <rubinradhakrishnan@gmail.com>
2020-09-16 22:17:45 +0530

    Add the shopping basket components

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./CheckoutProduct.css";
 
-function CheckoutProduct() {
+function CheckoutProduct({ id, image, title, price, rating }) {
   return (
     <div className="checkoutProduct">
-      <h2>I'm a checkoutProduct</h2>
+      <img className="checkoutProduct__image" src={image} alt="" />
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </div>
+        <button>Remove from Basket</button>
+      </div>
     </div>
   );
 }
